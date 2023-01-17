7c55f03881d48e1124dc22fc4d3dcdd20d59f282
Finn Drünert <finndruenert@gmail.com>
2023-01-17 22:34:21 +0100

    Add project files.

---
+++ PiIDE/JediCompletionWraper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiIDE {
+    internal static class JediCompletionWraper {
+        public static readonly string CodeCompleterPath = "C:\\Users\\finnd\\source\\repos\\PiIDE\\PiIDE\\Assets\\Jedi\\code_completer.exe";
+        private static readonly Process CompletionProcess;
+
+        static JediCompletionWraper() {
+            CompletionProcess = new Process() {
+                StartInfo = new ProcessStartInfo() {
+                    FileName = "cmd",
+                    Arguments = $"/c {CodeCompleterPath}",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    RedirectStandardInput = true,
+                    CreateNoWindow = true,
+                }
+            };
+            CompletionProcess.Start();
+        }
+
+        public static string GetCompletion(string filePath, int row, int col) {
+            CompletionProcess.StandardInput.WriteLine(filePath);
+            CompletionProcess.StandardInput.Flush();
+            CompletionProcess.StandardInput.WriteLine(row.ToString());
+            CompletionProcess.StandardInput.Flush();
+            CompletionProcess.StandardInput.WriteLine(col.ToString());
+            CompletionProcess.StandardInput.Flush();
+            string? line = CompletionProcess.StandardOutput.ReadLine();
+
+            if (line is null)
+                throw new NullReferenceException();
+
+            line = line.Substring(1);
+            return line;
+        }
+    }
+}
---
+++ PiIDE/TextEditor.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections;
+
+namespace PiIDE {
+
+    public partial class TextEditor : UserControl {
+
+        public TextEditor() {
+            InitializeComponent();
+
+            TextListBox.ItemsSource = GetDataSet();
+        }
+
+        private void TextEditor_PreviewKeyDown(object sender, KeyEventArgs e) {
+
+        }
+
+        public ArrayList GetDataSet() {
+            ArrayList items = new ArrayList();
+            for (var count = 0; count < 1000000; ++count) {
+                items.Add(string.Format("Item {0}", count));
+            }
+            return items;
+        }
+
+        public Point CaretPosition() => new();
+    }
+}
