7b88d789f84bcf4c527fe0fdeb0073aa2555a531
Finn Drünert <finndruenert@gmail.com>
2023-01-29 09:50:27 +0100

    Added Error Handling

--- PiIDE/App.xaml.cs
+++ PiIDE/App.xaml.cs
@@ -1,9 +1,36 @@
-﻿using System.Windows;
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Threading;
 
 namespace PiIDE {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
+
     public partial class App : Application {
+
+        public App() : base() {
+            Dispatcher.UnhandledException += OnDispatcherUnhandledException;
+        }
+
+        private void OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e) {
+            MessageBox.Show($"Unhandled exception occurred:\r\n{e.Exception.Message}\r\nYou can report this issue here: https://github.com/Finnomator/PiIDE/issues/new",
+                            "Error",
+                            MessageBoxButton.OK,
+                            MessageBoxImage.Error);
+            DumpUnhandledException(e);
+        }
+
+        private static void DumpUnhandledException(DispatcherUnhandledExceptionEventArgs e) {
+            string fileContent = "Exception: " + e.Exception.GetType()
+                                + "\r\nInnerException: " + e.Exception.InnerException
+                                + "\r\nMessage: " + e.Exception.Message
+                                + "\r\nStackTrace: " + e.Exception.StackTrace;
+
+            if (!Directory.Exists("Crashlogs"))
+                Directory.CreateDirectory("Crashlogs");
+
+            using FileStream fs = File.Create($"Crashlogs/{DateTime.Now:dd-MM-yyyy_HH-mm-ss}.txt");
+            fs.Write(new UTF8Encoding(true).GetBytes(fileContent));
+        }
     }
 }
